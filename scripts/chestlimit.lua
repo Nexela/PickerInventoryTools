53d74912995743ecdf082c32997cd7e806946532
Nicholas Dunn <nexela@gmail.com>
2018-08-19 23:49:35 -0400

    Fixes

--- scripts/chestlimit.lua
+++ scripts/chestlimit.lua
@@ -4,7 +4,7 @@
 local Event = require('__stdlib__/event/event')
 local Gui = require('__stdlib__/event/gui')
 local Player = require('__stdlib__/event/player')
-local Pad = require('__PickerExtended__/scripts/adjustment-pad')
+local Pad = require('__PickerExtended__/utils/adjustment-pad')
 
 local match_to_item = {
     ['container'] = true,
--- scripts/filterfill.lua
+++ scripts/filterfill.lua
@@ -6,7 +6,7 @@
 local Event = require('__stdlib__/event/event')
 local Gui = require('__stdlib__/event/gui')
 local Inventory = require('__stdlib__/entity/inventory')
-local lib = require('__PickerExtended__/scripts/lib')
+local lib = require('__PickerExtended__/utils/lib')
 
 --(( GUI ))--
 local INVENTORY_COLUMNS = 10
