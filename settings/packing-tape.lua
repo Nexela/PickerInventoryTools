b237fc6a0073290cf231f7e7a019f9c7782968f5
Nexela <nexela@gmail.com>
2020-10-24 15:51:15 -0400

    Optimize Packing Tape

--- settings/packing-tape.lua
+++ settings/packing-tape.lua
@@ -2,15 +2,15 @@
     "name": "packing-tape",
     "title": "Packing Tape",
     "author": "calcwizard",
-    "description": "Mining a chest or wagon allows players to pick it up with all the items inside and carry it in their inventory. Now supports cars!"
---]]
-if not mods['packing-tape'] then
-    data:extend {
-        {
-            name = 'picker-moveable-chests',
-            setting_type = 'startup',
-            type = 'bool-setting',
-            default_value = false
-        }
+    "description": "Mining a chest or wagon allows players to pick it up with all the items inside and carry
+                    it in their inventory. Now supports cars!"
+--]] --
+data:extend{
+    {
+        name = 'picker-moveable-chests',
+        setting_type = 'startup',
+        type = 'bool-setting',
+        default_value = false,
+        order = 'picker-moveable-chests'
     }
-end
+}
